a8ab6fce37b512a9d47ea1eb6638a1181c728ad4
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-10-06 16:38:51 +0200

    getting started

---
+++ src/BasicSyntax.java
@@ -0,0 +1,25 @@
+public class BasicSyntax {
+    public static void  main (String[] args) {
+        // this are the basic syntax in java for storing and getting datatypes.
+        /*
+        this style of comment is used for multiple lines and the one above is for
+        single comments
+         */
+
+
+        String name = "Ola Yinka";
+        int yearOfLearning = 2023;
+        byte months = 9;
+        String school = "IES Abastos";
+        String city = "Valencia";
+        double height = 5.7;
+        long milleAwayFromHome = 212757589829L;
+
+        System.out.println("Hello, World!");
+        System.out.printf("My name is %s, I am currently learning Java in %d, " +
+                "in %s, in the city of %s. Okay, I don't know what i meant by height %f but i know" +
+                " I am  %d way from home. Okay, that's a lie! lol",
+                name, yearOfLearning, school, city, height, milleAwayFromHome);
+
+    }
+}
