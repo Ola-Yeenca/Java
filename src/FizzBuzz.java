6d0e4cb5b0f85d24eede6021cf956457705cd3f0
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-10-08 14:08:39 +0200

    getting started

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,11 +12,13 @@ public class FizzBuzz {
         System.out.println("Please introduce the number: ");
         input = scanner.nextInt();
 
-        if (input % 5 == 0) {
+        if (input % 5 == 0 && input % 3 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (input % 5 == 0) {
             System.out.println("Fizz");
         } else if (input % 3 == 0) {
-            System.out.println("FizzBuzz");
-        }else
+            System.out.println("Buzz");
+        } else
             System.out.println(input);
 
         scanner.close();
