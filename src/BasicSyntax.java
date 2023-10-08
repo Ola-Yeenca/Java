0aac34182f3d942ef9bfb11178bc58c4e507f0fa
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-10-08 14:05:21 +0200

    getting started

--- src/BasicSyntax.java
+++ src/BasicSyntax.java
@@ -14,12 +14,15 @@ public class BasicSyntax {
         String city = "Valencia";
         double height = 5.7;
         long milleAwayFromHome = 212757589829L;
+        boolean guess = false;
 
         System.out.println("Hello, World!");
         System.out.printf("My name is %s, I am currently learning Java in %d, " +
                 "in %s, in the city of %s. Okay, I don't know what i meant by height %f but i know" +
-                " I am  %d way from home. Okay, that's a lie! lol",
-                name, yearOfLearning, school, city, height, milleAwayFromHome);
+                " I am  %d way from home. Okay, is all these true? Nah, some are %b%n! lol",
+                name, yearOfLearning, school, city, height, milleAwayFromHome, guess);
+
+
 
     }
 }
---
+++ src/FizzBuzz.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main (String[] args) {
+        /*
+         if the number is dividable by 5 print Fizz, if not, print  buzz; if it divisible
+         by both 3 and 5
+         print FizzBuzz and if it is not divisible by any, return the number.
+        */
+        int input;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please introduce the number: ");
+        input = scanner.nextInt();
+
+        if (input % 5 == 0) {
+            System.out.println("Fizz");
+        } else if (input % 3 == 0) {
+            System.out.println("FizzBuzz");
+        }else
+            System.out.println(input);
+
+        scanner.close();
+    }
+}
